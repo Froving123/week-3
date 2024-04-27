c5e5d659d75a9594622534daa59641e990473597
Froving123 <chernyh.sasha26@gmail.com>
2024-04-27 15:40:07 +0500

    Дэзэшка на проверку дубль два

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,17 +1,17 @@
 const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'localhost:3000'
-  ];
+  'https://practicum.yandex.ru',
+  'https://students-projects.ru',
+  'localhost:3000'
+];
 
-  function cors(req, res, next) {
+function cors(req, res, next) {
     const { origin } = req.headers;
     
-    if (allowedCors.includes(origin)) { 
+    if (allowedCors.includes(origin)) { // Если это наш друг
         res.header('Access-Control-Allow-Origin', origin);
     }
     
     next();
 }
 
-module.exports = {cors};
+module.exports = cors;
